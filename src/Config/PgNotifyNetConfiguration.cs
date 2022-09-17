ea8153530dd67f1fb59ed5d84dc0e4152b9176a9
psosnicki <peter.sosnicki@gmail.com>
2022-09-17 10:45:50 +0200

    configuration and extensions

---
+++ src/Config/PgNotifyNetConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using PgNotifyNet.Builders;
+
+namespace PgNotifyNet;
+
+public record PgNotifyNetOptions(bool RemoveTriggersOnShutdown = false);
+
+public interface IPostgresNotifyConfiguration : IPgNotifyConfigurator
+{
+    IReadOnlyCollection<Trigger> Triggers { get; }
+    string ConnectionString { get; }
+    PgNotifyNetOptions Options { get; }
+}
+
+public interface IPgNotifyConfigurator
+{
+    IPgNotifyConfigurator Trigger(Func<ITriggerOnTable, Trigger> triggerBuilder);
+}
+
+public class PgNotifyNetConfiguration : IPostgresNotifyConfiguration
+{
+    public IReadOnlyCollection<Trigger> Triggers => _triggers.AsReadOnly();
+    public string ConnectionString { get; }
+    public PgNotifyNetOptions Options { get; }
+
+    private readonly List<Trigger> _triggers = new();
+
+    internal PgNotifyNetConfiguration(string connectionString, PgNotifyNetOptions options)
+    {
+        ConnectionString = connectionString;
+        Options = options;
+    }
+
+    public IPgNotifyConfigurator Trigger(Func<ITriggerOnTable, Trigger> triggerBuilder)
+    {
+        _triggers.Add(triggerBuilder.Invoke(new TriggerBuilder()));
+        return this;
+    }
+}
---
+++ src/Extensions/PgNotifyNetExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PgNotifyNet.Interfaces;
+using PgNotifyNet.Services;
+using System.Reflection;
+
+namespace PgNotifyNet.Extensions;
+
+public static class PgNotifyNetExtensions
+{
+    public static IServiceCollection AddPgNotifyNet(this IServiceCollection serviceCollection, Assembly[] assemblies, string connectionString, Action<IPgNotifyConfigurator> configurator, PgNotifyNetOptions? options = null)
+        => AddPgNotify(serviceCollection, assemblies, connectionString, configurator, options);
+
+    public static IServiceCollection AddPgNotifyNet(this IServiceCollection serviceCollection, string connectionString, Action<IPgNotifyConfigurator> configurator, PgNotifyNetOptions? options = null)
+        => AddPgNotify(serviceCollection, AppDomain.CurrentDomain.GetAssemblies(), connectionString, configurator, options);
+
+    private static IServiceCollection AddPgNotify(this IServiceCollection serviceCollection, Assembly[] assemblies, string connectionString, Action<IPgNotifyConfigurator> configurator, PgNotifyNetOptions? options = null)
+    {
+        assemblies.SelectMany(x => x.GetTypes())
+              .Where(item => item.GetInterfaces()
+              .Where(i => i.IsGenericType).Any(i => i.GetGenericTypeDefinition() == typeof(IHandleNotification<>)) && !item.IsAbstract && !item.IsInterface)
+              .ToList()
+              .ForEach(assignedType =>
+              {
+                  var serviceType = assignedType.GetInterfaces().FirstOrDefault(i => i.GetGenericTypeDefinition() == typeof(IHandleNotification<>));
+                  if (serviceType != null)
+                      serviceCollection.AddTransient(serviceType, assignedType);
+              });
+
+        serviceCollection.AddSingleton<IPgNotificationService, PgNotificationService>();
+        serviceCollection.AddSingleton<IPostgresNotifyConfiguration>(_ =>
+        {
+
+            var pqNotify = new PgNotifyNetConfiguration(connectionString, options ?? new PgNotifyNetOptions());
+            configurator.Invoke(pqNotify);
+            return pqNotify;
+        });
+
+        serviceCollection.AddHostedService(sp => new PgNotifyNetHostService(sp.GetRequiredService<IPgNotificationService>(), sp.GetRequiredService<IPostgresNotifyConfiguration>()));
+        return serviceCollection;
+    }
+}
